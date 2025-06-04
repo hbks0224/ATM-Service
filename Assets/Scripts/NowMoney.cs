66ea01ff77b0e1265c2537fdf9b279daa828b823
hbks0224 <hbks1234@gmail.com>
2025-06-04 13:10:05 +0900

    Initial commit

---
+++ Assets/Scripts/NowMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NowCash : MonoBehaviour
+{
+    public int nowmoney = 0;
+
+    string nowmoneyStr;
+    public TextMeshProUGUI textMeshProUGUI;
+
+    private void Start()
+    {
+        nowmoneyStr = nowmoney.ToString("#,##0");
+        textMeshProUGUI.text = nowmoneyStr;
+    }
+
+
+
+
+}
