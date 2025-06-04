68d2b24d08ee86b6a1d703683dd6277ff50a9337
hbks0224 <hbks1234@gmail.com>
2025-06-04 15:50:12 +0900

    feat : 입출금 창 이동 구현
    입금 버튼을 누르면 입금 화면이, 출금 버튼을 누르면 출금 화면이 뜨게 버튼 이벤트 연결하기

---
+++ Assets/Scripts/PopupBank.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupBank : MonoBehaviour
+{
+    public Button InputBtn;
+    public Button OutputBtn;
+    public Button InputBackBtn;
+    public Button OutputBackBtn;
+    public GameObject InputUI;
+    public GameObject OutputUI;
+    public GameObject ATMUI;
+
+   public void OnInput()
+    {
+
+        InputUI.SetActive(true);
+        OutputUI.SetActive(false);
+        ATMUI.SetActive(false);
+    }
+
+    public void OnOutput() 
+    { 
+        OutputUI.SetActive(true);
+        InputUI.SetActive(false);
+        ATMUI.SetActive(false);
+    
+    }
+    
+    public void GoBack()
+    {
+
+        InputUI.SetActive(false);
+        OutputUI.SetActive(false);
+        ATMUI.SetActive(true);
+    }
+
+
+
+}
