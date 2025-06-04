fbd26ffb758982abf4826364fbeb093e2c1e08d4
hbks0224 <hbks1234@gmail.com>
2025-06-04 14:37:23 +0900

    feat : 유저 데이터 제작, 게임 매니저 제작 데이터와 UI 연동
    유저 정보 데이터를 담기 위한 UserData 클래스 작성
    GameManager를 싱글톤으로 만들고 UserData 클래스 변수를 생성 및 데이터 초기화
    게임 실행 시 집어넣은 데이터가 UI에 반영되도록 연결

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public UserData userData;
+
+    public TextMeshProUGUI NameText;
+    public TextMeshProUGUI BalanceText;
+    public TextMeshProUGUI CashText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        userData = new UserData("HyeokJun", 85000, 115000);
+        Refresh();
+
+    }
+    public void Refresh() 
+    
+    {
+        NameText.text = userData.Username;
+        BalanceText.text = userData.AccountBalance.ToString("#,##0");
+        CashText.text = userData.Cash.ToString("#,##0");
+    
+    
+    }
+
+}
---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UserData
+{
+
+   public string Username ; //사용자명
+   public int Cash; //현금
+   public int AccountBalance; // 통장 잔고
+
+    public UserData(string username, int cash, int accountBalance)
+    {
+        Username = username;
+        Cash = cash;
+        AccountBalance = accountBalance;
+
+
+    }
+
+}
