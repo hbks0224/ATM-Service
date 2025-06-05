f429228b045859e67102734e5978cee79f4ff942
hbks0224 <hbks1234@gmail.com>
2025-06-05 10:30:25 +0900

    feat : 송금 기능 구현

--- Assets/Scripts/PopupBank.cs
+++ Assets/Scripts/PopupBank.cs
@@ -28,6 +28,12 @@ public class PopupBank : MonoBehaviour
 
     public TMP_InputField IdBox;
     public TMP_InputField PSBox;
+
+    public TMP_InputField Receiver;
+    public TMP_InputField Amount;
+    public GameObject TransferUI;
+    public GameObject Transfer_CheckInfoPopup;
+    public TextMeshProUGUI Transfer_CheckInfoPopup_Text;
     private void Start()
     {
         LoginUI.SetActive(true);
@@ -49,18 +55,19 @@ public class PopupBank : MonoBehaviour
     }
 
     public void OnOutput() // 출금 버튼 액션
-    { 
+    {
         OutputUI.SetActive(true);
         InputUI.SetActive(false);
         ATMUI.SetActive(false);
-    
+
     }
-    
+
     public void GoBack() // 뒤로가기 버튼 액션
     {
 
         InputUI.SetActive(false);
         OutputUI.SetActive(false);
+        TransferUI.SetActive(false);
         ATMUI.SetActive(true);
     }
 
@@ -68,7 +75,7 @@ public class PopupBank : MonoBehaviour
     public void Input(int input)
     {
 
-        if(GameManager.Instance.userData.Cash - input < 0)
+        if (GameManager.Instance.userData.Cash - input < 0)
         {
             OnPopup();
             return;
@@ -78,13 +85,13 @@ public class PopupBank : MonoBehaviour
         GameManager.Instance.SaveUserData();
         GameManager.Instance.Refresh();
 
-        
+
 
     }
     public void InputCustom()
     {
         int amount;
-        if(int.TryParse(InputField.text, out amount))
+        if (int.TryParse(InputField.text, out amount))
         {
 
             if (GameManager.Instance.userData.Cash - amount < 0)
@@ -145,7 +152,7 @@ public class PopupBank : MonoBehaviour
 
         Popup.SetActive(false);
     }
-    
+
     public void OnSignUpPopUp()
     {
 
@@ -154,49 +161,49 @@ public class PopupBank : MonoBehaviour
     public void SignUpCancel()
     {
 
-        SignUpPopup.SetActive(false );
+        SignUpPopup.SetActive(false);
     }
     public void MakeAccount()
     {
-        if (SingupID.text == "" || SingupName.text == "" || SingupPS.text == ""|| SingupConfim.text =="")
+        if (SingupID.text == "" || SingupName.text == "" || SingupPS.text == "" || SingupConfim.text == "")
         {
             CheckInfoPopup.SetActive(true);
             ErrorMassage.text = "다음 사항이 빠져있습니다 : ";
-            if(SingupID.text == "")
+            if (SingupID.text == "")
             {
 
                 ErrorMassage.text += "ID ";
             }
 
-            if(SingupName.text == "")
+            if (SingupName.text == "")
             {
 
                 ErrorMassage.text += "이름 ";
 
             }
 
-            if(SingupPS.text == "")
+            if (SingupPS.text == "")
             {
 
                 ErrorMassage.text += "비밀번호 ";
             }
 
-            if(SingupConfim.text == "")
+            if (SingupConfim.text == "")
             {
 
                 ErrorMassage.text += "비밀번호 확인 ";
             }
 
-            
 
-            
+
+
         }
 
 
 
         else
         {
-            if(SingupPS.text == SingupConfim.text)
+            if (SingupPS.text == SingupConfim.text)
             {
 
                 if (PlayerPrefs.HasKey($"{SingupID.text}"))
@@ -223,7 +230,7 @@ public class PopupBank : MonoBehaviour
                 }
 
 
-                
+
             }
             else
             {
@@ -231,7 +238,7 @@ public class PopupBank : MonoBehaviour
                 ErrorMassage.text = "비밀번호가 틀립니다.";
             }
         }
-        
+
 
 
 
@@ -240,15 +247,20 @@ public class PopupBank : MonoBehaviour
 
     public void OffCheckInfoPopUp()
     {
-        CheckInfoPopup.SetActive(false ) ; 
+        CheckInfoPopup.SetActive(false);
 
     }
-    
+    public void OffTransfer_CheckInfoPopUp()
+    {
+        Transfer_CheckInfoPopup.SetActive(false);
+
+    }
+
     public void Login()
     {
         Debug.Log(PlayerPrefs.GetString($"{IdBox.text}"));
-        Debug.Log( PlayerPrefs.GetString($"{IdBox.text}/Password"));
-        if(IdBox.text == PlayerPrefs.GetString($"{IdBox.text}") && PSBox.text == PlayerPrefs.GetString($"{IdBox.text}/Password"))
+        Debug.Log(PlayerPrefs.GetString($"{IdBox.text}/Password"));
+        if (IdBox.text == PlayerPrefs.GetString($"{IdBox.text}") && PSBox.text == PlayerPrefs.GetString($"{IdBox.text}/Password"))
         {
             userData.Username = PlayerPrefs.GetString($"{IdBox.text}/UserName", userData.Username);
             userData.Cash = PlayerPrefs.GetInt($"{IdBox.text}/Cash", userData.Cash);
@@ -256,12 +268,65 @@ public class PopupBank : MonoBehaviour
             userData.Password = PlayerPrefs.GetString($"{IdBox.text}/Password", userData.Password);
             userData.ID = PlayerPrefs.GetString($"{IdBox.text}", userData.ID);
 
-            LoginUI.SetActive(false );
-            ATMUI.SetActive(true); 
+            LoginUI.SetActive(false);
+            ATMUI.SetActive(true);
             UserInfoUI.SetActive(true);
             GameManager.Instance.LoadUserData(IdBox.text);
             GameManager.Instance.Refresh();
 
         }
+    }
+
+    public void Transfer()
+    {
+        if (Receiver.text == "" || Amount.text == "")
+        {
+            Transfer_CheckInfoPopup.SetActive(true);
+            Transfer_CheckInfoPopup_Text.text = "입력 정보를 확인해주세요.";
+            return;
+
+        }
+
+
+        if (Receiver.text != PlayerPrefs.GetString($"{Receiver.text}/UserName")
+            && Receiver.text != PlayerPrefs.GetString($"{Receiver.text}"))
+        {
+            Transfer_CheckInfoPopup.SetActive(true);
+            Transfer_CheckInfoPopup_Text.text = "대상이 없습니다.";
+            return;
+
+        }
+        int amount;
+        if (int.TryParse(Amount.text, out amount)) // int 범위 넘어가면 에러 발생함
+        {
+            if (GameManager.Instance.userData.Cash - amount < 0)
+            {
+
+                Transfer_CheckInfoPopup.SetActive(true);
+                Transfer_CheckInfoPopup_Text.text = "잔액이 부족합니다.";
+                return;
+            }
+        }
+
+        //성공case
+        GameManager.Instance.userData.Cash -= amount;
+        int ReceiveMoney = PlayerPrefs.GetInt($"{Receiver.text}/Cash");
+        ReceiveMoney += amount;
+
+        PlayerPrefs.SetInt($"{Receiver.text}/Cash", ReceiveMoney);
+        Debug.Log("송금 완료");
+        GameManager.Instance.SaveUserData();
+        GameManager.Instance.Refresh();
+
+
+
+    }
+
+    public void OnTransfer()
+    {
+
+
+        TransferUI.SetActive(true);
+        ATMUI.SetActive(false);
     }
 }
